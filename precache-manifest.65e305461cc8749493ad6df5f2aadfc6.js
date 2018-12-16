371e2a157f2ff3270acc71e66dfb2377db0801f3
student08 <andrewgaryga@gmail.com>
2018-12-16 22:10:57 +0200

    Initial commit
    Initial commit
    
    build for heroku

---
+++ precache-manifest.65e305461cc8749493ad6df5f2aadfc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2f51f6a19691a9a6226e",
+    "url": "/static/js/main.2f51f6a1.chunk.js"
+  },
+  {
+    "revision": "5c0f5383619aa043fe9f",
+    "url": "/static/js/1.5c0f5383.chunk.js"
+  },
+  {
+    "revision": "2f51f6a19691a9a6226e",
+    "url": "/static/css/main.0017a159.chunk.css"
+  },
+  {
+    "revision": "5c0f5383619aa043fe9f",
+    "url": "/static/css/1.099a9717.chunk.css"
+  },
+  {
+    "revision": "6b440d287f49b48e2c83c86c320c4e7f",
+    "url": "/index.html"
+  }
+];
